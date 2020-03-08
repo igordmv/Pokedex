fa4c2687019e8ad2629074c37c25171290a82fa5
Igor Vieira <igor_recreio23@hotmail.com>
2020-03-08 16:06:23 -0300

    setup search view on main activity

--- app/src/main/java/com/idv/pokedex/view/MainActivity.kt
+++ app/src/main/java/com/idv/pokedex/view/MainActivity.kt
@@ -1,13 +1,82 @@
 package com.idv.pokedex.view
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.util.TypedValue
+import android.view.Menu
+import android.view.MenuInflater
+import android.widget.EditText
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
 import com.idv.pokedex.R
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener, TextWatcher {
+
+    private lateinit var searchView: SearchView
+    private lateinit var searchViewText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.setSupportActionBar(searchToolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_searchview, menu)
+        searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        configureSearchView()
+        return true
+    }
+
+    private fun configureSearchView() {
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager?
+        searchView.setSearchableInfo(searchManager?.getSearchableInfo(componentName))
+        searchView.setIconifiedByDefault(false)
+        searchView.setOnQueryTextListener(this)
+
+        searchViewText = searchView.findViewById(androidx.appcompat.R.id.search_src_text)
+        searchViewText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
+
+            searchViewText.setHintTextColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.toolbar_text_color
+                )
+            )
+        searchViewText.addTextChangedListener(this)
+        searchView.requestFocus()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (Intent.ACTION_SEARCH == intent?.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+
+        }
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
+    }
+
 }
